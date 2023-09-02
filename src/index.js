201564041595acb24940b0bf1ade01a27f793e7c
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-09-02 17:33:56 +0300

    Create api for adding name and scores

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import './index.css';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import _ from 'lodash';
+import { createData, getData } from './modules/api.js';
+
+const refreshBtn = document.getElementById('refresh');
+const submitBtn = document.getElementById('submit');
+
+const render = async () => {
+  const data = await getData();
+  const recentScore = document.getElementById('list-ul');
+  recentScore.innerHTML = '';
+  const sortedData = _.sortBy(data, 'score');
+  sortedData.forEach((item) => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('list-item');
+    listItem.innerText = `${item.user}: ${item.score}`;
+    recentScore.appendChild(listItem);
+  });
+};
+
+refreshBtn.addEventListener('click', render);
+
+submitBtn.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const nameInput = document.getElementById('name-input');
+  const scoreInput = document.getElementById('score-input');
+  const data = {
+    user: nameInput.value,
+    score: Number(scoreInput.value),
+  };
+  await createData(data);
+  nameInput.value = '';
+  scoreInput.value = '';
+});
